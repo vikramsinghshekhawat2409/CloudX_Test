680dd23b8ee858cb1de176f5b2ee7b9d113cd7b6
vikramsinghshekhawat2409 <vikramss0264@gmail.com>
2020-01-19 21:50:12 +0530

    Changes in models to register new models and mapping of projects urls to accounts/urls.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from accounts.models import UserProfileInfo, User
 # Register your models here.
+admin.site.register(UserProfileInfo)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(blank=True,max_length=13)
+    def __str__(self):
+        """
+        :return: user's username
+        """
+        return self.user.username
