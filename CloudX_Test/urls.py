4952495b24222b023cc4de385ca4ca361c14e0cc
vikramsinghshekhawat2409 <vikramss0264@gmail.com>
2020-01-19 21:56:02 +0530

    Changes in views.py and project urls to include function index and addition of templates for home page

--- CloudX_Test/urls.py
+++ CloudX_Test/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/',include('accounts.urls')),
+    url(r'^$',views.index,name='index'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    """
+    :param request:
+    :return: returns rendered html template with welcome message including an username if the user is logged in
+             and if not so returns rendered html asking user to register or log in
+             and if any exception raises returns an internal server error with the error
+    """
+    try:
+        return render(request,'accounts/index.html')
+    except Exception as e:
+        return HttpResponse(e, status=500)
